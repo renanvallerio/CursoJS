1028836b309b733896264acf926be0969a76b7ef
renanvallerio <renanvallerio@gmail.com>
2022-08-12 17:52:11 -1000

    aula4

---
+++ aula4/comentarios.js
@@ -0,0 +1,9 @@
+// And write a long comment here
+console.log('Hello, World!'); // Here is other comment
+console.log('Line 1');
+/* 
+//console.log('Line 2');
+console.log('Line 3');
+console.log('Line 4');
+ */
+// Will be ignored
