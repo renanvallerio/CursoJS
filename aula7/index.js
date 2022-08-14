11fbcb3b1b830045462e4d5454250d2c7d5d434e
renanvallerio <renanvallerio@gmail.com>
2022-08-13 18:07:14 -1000

    aula7

---
+++ aula7/index.js
@@ -0,0 +1,32 @@
+// String = Text | Number = Number
+const firstNumber = 5;
+const secondNumber = 10;
+const result = firstNumber * secondNumber;
+const duplicatedResult = result * 2;
+let triplicatedResult = result *3;
+triplicatedResult = triplicatedResult + 5;
+
+console.log(typeof(firstNumber + secondNumber));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos CamelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
