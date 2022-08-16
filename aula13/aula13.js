eee51124c4c41fb2f27e1637de5fad9f4a7e42ba
renanvallerio <renanvallerio@gmail.com>
2022-08-15 15:55:48 -1000

    aula13

---
+++ aula13/aula13.js
@@ -0,0 +1,8 @@
+//               0123456789
+let umaString = 'O rato roeu a roupa do rei de roma'
+
+console.log(umaString.toLowerCase())
+console.log(umaString.toUpperCase())
+console.log(umaString.split(' '))
+console.log(umaString.slice(11))
+
