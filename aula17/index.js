e6e2933dd8e1b772f5be7f30cdd8e6b2f1d523b1
renanvallerio <renanvallerio@gmail.com>
2022-08-16 22:33:16 -1000

    aula17

---
+++ aula17/index.js
@@ -0,0 +1,26 @@
+// function soma(x = 1, y = 1) {
+//   const resultado = x + y
+//   return resultado
+// }
+
+// const resultado = soma(4, 2)
+// console.log(resultado);
+
+// Função Anônima
+// const raiz = function (n) {
+//   return n ** 0.5
+// }
+
+// Arrow Function
+const raiz = (n) => n ** 0.5
+
+console.log(raiz(1))
+console.log(raiz(4))
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+console.log(raiz(36))
+console.log(raiz(49))
+console.log(raiz(64))
+console.log(raiz(81))
+console.log(raiz(100))
