ee155ac07f2d97e06c5c8c9b43d0e51728009512
renanvallerio <renanvallerio@gmail.com>
2022-08-16 21:56:56 -1000

    aula16

---
+++ aula16/index.js
@@ -0,0 +1,40 @@
+//               0       1        2         3      4
+let alunos = ['Luiz', 'Maria', 'João'] // Luiza, Eduardo
+alunos = 123
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+
+// console.log(alunos.slice(0, -2))
+
+// console.log(alunos[50]);
+
+// delete alunos[1]
+// console.log(alunos[1]);
+
+// const removido = alunos.shift()
+// console.log(removido)
+// console.log(alunos)
+
+// Adiciona no começo
+// alunos.unshift('Fábio')
+// alunos.unshift('Luiza')
+
+// Adiciona no fim
+// alunos.push('Luiza')
+// alunos.push('Fábio')
+
+// Adiciona no fim
+// alunos[alunos.length] = 'Luiza'
+// alunos[alunos.length] = 'Fábio'
+// alunos[alunos.length] = 'Luana'
+
+// alunos[0] = 'Eduardo' // Altera
+// alunos[3] = 'Luiza' // Adiciona
+
+// console.log(alunos)
+// console.log(alunos[0]);
+// console.log(alunos[2]);
