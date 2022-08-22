eb2d3d090f295273abfd0c4b8042b6238b55abd0
renanvallerio <renanvallerio@gmail.com>
2022-08-21 21:19:48 -1000

    aula28

--- aula28/index.js
+++ aula28/index.js
@@ -17,16 +17,6 @@ const data = new Date()
 const dataBrasil = formataData(data)
 console.log(dataBrasil)
 
-
-
-
-
-
-
-
-
-
-
 // const dezHoras = 60 * 60 * 10 * 1000
 // const umDia = 60 * 60 * 24 * 1000
 // const data = new Date(0 + dezHoras - umDia) // 01/01/1970 Timestamp unix ou época unix
