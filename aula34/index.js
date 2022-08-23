e1ac3d1f602f24e4f8d81d3be0f949130eb69277
renanvallerio <renanvallerio@gmail.com>
2022-08-22 22:12:11 -1000

    aula34

---
+++ aula34/index.js
@@ -0,0 +1,16 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// // i - index
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`Linha ${i}`);
+// }
+
+let frutas = ['Melancia', 'Maçã', 'Pera', 'Uva', 'Laranja', 'Banana', 'Abacaxi', 'Manga', 'Kiwi', 'Mamão']
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Indice ${i} ${frutas[i]}`)
+}
