0635b5c8c9f13dc358c888881f104b0a93aa9e3b
renanvallerio <renanvallerio@gmail.com>
2022-08-25 16:48:33 -1000

    aula42

---
+++ aula42/index.js
@@ -0,0 +1,8 @@
+// Escreva uma função chamada ePaisagem
+// Que recebe dois argumentos, largura e altura de uma imagem (number).
+// Retorne true se a imagem estiver no modo paisagem
+
+const ePaisagem = (width, height) => width > height
+
+console.log(ePaisagem(1920, 1080))
+console.log(ePaisagem(1080, 1920))
