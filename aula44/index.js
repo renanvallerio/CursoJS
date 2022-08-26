0698a5e675eb54e40676c853559b598264662399
renanvallerio <renanvallerio@gmail.com>
2022-08-25 22:28:06 -1000

    aula44

---
+++ aula44/index.js
@@ -0,0 +1,13 @@
+function soma(x, y) {
+  if (typeof x !== 'number' || typeof x !== 'number')
+    throw new Error('x e y precisam ser números')
+  return x + y
+}
+
+try {
+  console.log(soma(2, 2))
+  console.log(soma('1', 2))
+} catch (error) {
+  console.log(error)
+  console.log('Alguma coisa mais amigável para o usuário.')
+}
