2c7c27b94cbcd598d70faa1916c15272843b3f50
renanvallerio <renanvallerio@gmail.com>
2022-08-27 21:51:44 -1000

    aula49

---
+++ aula49/index.js
@@ -0,0 +1,41 @@
+// Declaração de função (Function hoisting)
+falaOi()
+function falaOi() {
+  console.log('Oi')
+}
+
+// First-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+  console.log('Sou um dado.')
+}
+souUmDado()
+
+// Arrow function
+const funcaoArrow = () => {
+  console.log('Sou uma arrow function')
+}
+funcaoArrow()
+
+// Dentro de um objeto
+const obj = {
+  // falar: function () {
+  falar() {
+    console.log('Estou falando...')
+  }
+}
+obj.falar()
+
+// Teste
+let i = 0
+
+const maisMais = () => i++
+
+const repeat = setInterval(() => {
+  maisMais()
+  console.log(i)
+}, 1000)
+
+setTimeout(() => {
+  clearInterval(repeat)
+}, 6000)
