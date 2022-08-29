3dea1066ea58d8e650d648faf55f871b10ec86b0
renanvallerio <renanvallerio@gmail.com>
2022-08-28 21:29:12 -1000

    aula53

---
+++ aula53/index.js
@@ -0,0 +1,14 @@
+//Global
+function retornaFuncao(nome) {
+  // const nome = 'Luiz'
+  return function () {
+    return nome
+  }
+}
+
+const funcao = retornaFuncao('Luiz')
+const funcao2 = retornaFuncao('João')
+console.dir(funcao)
+console.dir(funcao2)
+
+console.log(funcao(), funcao2());
