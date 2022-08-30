7dbabf5ede6475e3e393b4b9bff62060f8f025ee
renanvallerio <renanvallerio@gmail.com>
2022-08-29 17:09:52 -1000

    aula55

---
+++ aula55/index.js
@@ -0,0 +1,14 @@
+// IIFE - Immediatly Invoked function expression
+;(function (idade, peso, altura) {
+  const sobrenome = 'Miranda'
+  function criaNome(nome) {
+    return nome + ' ' + sobrenome
+  }
+
+  function falaNome() {
+    console.log(criaNome('Luiz'))
+  }
+
+  falaNome()
+  console.log(idade, peso, altura)
+})(30, 80, 1.8)
