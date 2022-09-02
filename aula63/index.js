b3bf151a62f7a440d9086007e119d383b9517f03
renanvallerio <renanvallerio@gmail.com>
2022-09-01 17:59:03 -1000

    aula63

---
+++ aula63/index.js
@@ -0,0 +1,18 @@
+//             -5       -4      -3         -2         -1
+//              0        1       2          3          4
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
+
+//push
+// nomes.splice(nomes.length, 0, 'Luiz', 'Otávio', 'Miranda')
+// console.log(nomes)
+
+//unshift
+nomes.splice(0, 0, 'Luiz', 'Otávio')
+console.log(nomes)
+
+// nomes.splice(índice atual, delete, elem1, elem2, elem3)
+//pop
+// const removidos = nomes.splice(-1, 1)
+
+//shift
+// const removidos = nomes.splice(0, 1)
