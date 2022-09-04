60a89b50b5ad55bb6069ca74c45a51b64c924c79
renanvallerio <renanvallerio@gmail.com>
2022-09-03 21:55:04 -1000

    aula70

---
+++ aula70/index.js
@@ -0,0 +1,11 @@
+// Factory functions / Constructor functions / Classes
+function Pessoa(nome, sobrenome) {
+  ;(this.nome = nome), (this.sobrenome = sobrenome), Object.freeze(this)
+}
+
+const p1 = new Pessoa('Luiz', 'Miranda')
+delete p1.nome
+const p2 = new Pessoa('Maria', 'Miranda')
+
+console.log(p1)
+console.log(p2)
