52ddcbb465668303231955cc2a100d3ce2c630bb
renanvallerio <renanvallerio@gmail.com>
2022-09-08 22:59:59 -1000

    aula77

--- aula77/index.js
+++ aula77/index.js
@@ -53,18 +53,72 @@ function ValidaCPF(cpf) {
     return conta <= 9 ? conta : 0
   }
 
+  this.isSequencia = () => {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo
+  }
+
   this.cpfIsValid = () => {
     let cpfArray = Array.from(this.cpfLimpo)
-    let cpfMenosDois = cpfArray.slice(0, -2)
-//
+    let cpfMenosDois = cpfArray.slice(0, -2).toString().replaceAll(',', '')
     let cpfPronto = cpfMenosDois + this.primeiroDigito() + this.segundoDigito()
-    console.log(cpfPronto)
-    console.log(this.cpfLimpo)
+    if (this.isSequencia()) return false
+
     return cpfPronto === this.cpfLimpo
   }
 }
 
 const luiz = new ValidaCPF('705.484.450-52')
-console.log(luiz.primeiroDigito())
-console.log(luiz.segundoDigito())
+// console.log(luiz.primeiroDigito())
+// console.log(luiz.segundoDigito())
 console.log(luiz.cpfIsValid())
+
+// Correção
+
+function ValidaCPF2(cpfEnviado) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function () {
+      return cpfEnviado.replace(/\D+/g, '')
+    }
+  })
+}
+
+ValidaCPF2.prototype.valida = function () {
+  if (typeof this.cpfLimpo === 'undefined') return false
+  if (this.cpfLimpo.length !== 11) return false
+  if (this.isSequencia()) return false
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2)
+  const digito1 = this.criaDigito(cpfParcial)
+  const digito2 = this.criaDigito(cpfParcial + digito1)
+
+  const cpfNovo = cpfParcial + digito1 + digito2
+  return cpfNovo === this.cpfLimpo
+}
+
+ValidaCPF2.prototype.criaDigito = function (cpfParcial) {
+  const cpfArray = Array.from(cpfParcial)
+
+  let regressivo = cpfArray.length + 1
+  const total = cpfArray.reduce((ac, val) => {
+    ac += regressivo * Number(val)
+    regressivo--
+    return ac
+  }, 0)
+
+  const digito = 11 - (total % 11)
+  return digito > 9 ? '0' : String(digito)
+}
+
+ValidaCPF2.prototype.isSequencia = function () {
+  const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+  return sequencia === this.cpfLimpo
+}
+
+const cpf = new ValidaCPF2('705.484.450-52')
+if (cpf.valida()) {
+  console.log('CPF VÁLIDO')
+} else {
+  console.log('CPF INVÁLIDO')
+}
